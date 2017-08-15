09c8fcfe6a67de45fae79a3750ec87a4db63e8a8
Weerapon <top.collection.it@gmail.com>
2017-08-16 00:48:35 +0700

    input from user and learning if-else

--- main.go
+++ main.go
@@ -9,17 +9,26 @@ func main() {
   // fmt.Println("Hello, Gopher.")
 
   // Declare variable
-  var gopher string
-  gopher = "Gopher"
-  fmt.Printf("Hello, %s.\n", gopher)
+  // var gopher string
+  // gopher = "Gopher"
+  // fmt.Printf("Hello, %s.\n", gopher)
 
-  var name = "weerapon top"
-  fmt.Printf("My name is %s. \n", name)
+  // var name = "weerapon top"
+  // fmt.Printf("My name is %s. \n", name)
 
-  nickname := "TOPz"
+  // nickname := "TOPz"
   // fmt.Printf("My nickname is %s. \n", nickname)
   
   // fix compile error case declare vaialble not used 
   // _ not keep value
-  _ = nickname
+  // _ = nickname
+
+  fmt.Print("Input a fruit: ")
+  var fruit string
+  fmt.Scanln(&fruit)
+
+  if fruit == "" {
+    fmt.Println("meh! ")
+    return
+  }
 }

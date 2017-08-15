e45d6d763d68c29470161ee271daadc8e1dc3a08
Weerapon <top.collection.it@gmail.com>
2017-08-16 00:41:15 +0700

    declare variable

--- main.go
+++ main.go
@@ -5,5 +5,14 @@ import (
 )
 
 func main() {
-  fmt.Println("Hello, Gopher.")
+  // print
+  // fmt.Println("Hello, Gopher.")
+
+  // Declare variable
+  var gopher string
+  gopher = "Gopher"
+  fmt.Printf("Hello, %s.\n", gopher)
+
+  var name = "weerapon top"
+  fmt.Printf("My name is %s. \n", name)
 }
